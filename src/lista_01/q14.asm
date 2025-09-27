9dade434b3a028333d813c352a986e075c64bb99
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-09-27 15:51:42 -0300

    corrige a formatação do código

--- src/lista_01/q14.asm
+++ src/lista_01/q14.asm
@@ -4,11 +4,10 @@ main: addi $2,$0,5
       syscall
       add $8,$0,$2
       
-      
       andi $12,$8,1
       
       add $4,$0,$12
-   	 addi $2, $0, 1
+   	  addi $2, $0, 1
       syscall
        
       addi $2, $0, 10
