7324681bd7bcaae07f7ed5152dc3cfcf28b67562
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-09-27 15:38:41 -0300

    feat: adiciona questão de paridade

---
+++ src/lista_01/q14.asm
@@ -0,0 +1,15 @@
+.text
+
+main: addi $2,$0,5
+      syscall
+      add $8,$0,$2
+      
+      
+      andi $12,$8,1
+      
+      add $4,$0,$12
+   	 addi $2, $0, 1
+      syscall
+       
+      addi $2, $0, 10
+      syscall
