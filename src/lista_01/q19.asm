59265aa5ef928bb2614b7c91248be67683f17f7d
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-09-27 13:30:10 -0300

    feat: adiciona questão calendario edulandes

--- src/lista_01/q19.asm
+++ src/lista_01/q19.asm
@@ -1,3 +1,21 @@
+# Em Edulândia o calendário é muito parecido com o nosso, exceto pelo fato de não existirem anos bissextos e o mês de fevereiro ter uma quantidade fixa de dias, isso é, 30. Faça um programa que leia um inteiro representando um mês do ano Edulandês e imprima a quantidade de dias que tem esse mês.
 .text
-
-main:
+main:  addi $2, $0, 5
+       syscall
+       add $8, $0, $2
+       
+       addi $9, $0, 8
+       div $8, $9
+       
+       mflo $10
+       
+       add $11, $8, $10
+       
+       andi $12, $11, 1  
+       
+       addi $4, $12, 30
+       addi $2, $0, 1
+       syscall
+       
+       addi $2, $0, 10
+       syscall
