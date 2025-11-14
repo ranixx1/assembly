24439cb1e48d2f2d3f04b9bd2195ac2b36a4523d
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-11-14 00:01:15 -0300

    feat: add macro loop com sw e lw

---
+++ src/test/exe/input_print_vetor.asm
@@ -0,0 +1,39 @@
+.data
+.text
+
+main: addi $8,$0,10
+      addi $9,$0,0
+      lui $10, 0x1000
+      
+for1: beq $9,$8, for2
+      addi $2,$0,5
+      syscall
+
+      sw $2,0($10)
+
+      addi $10,$10,4
+      addi $9,$9,1
+      j for1
+
+for2: lui $10,0x1000
+
+      addi $9,$0,0
+
+
+p_for2: 
+      beq $9,$8,fim
+
+      lw $4,0($10)
+      addi $2,$0,1
+      syscall
+
+      addi $4,$0,'\n'
+      addi $2,$0,11
+      syscall
+            
+      addi $10,$10,4
+      addi $9,$9,1
+      j p_for2
+
+fim: addi $2,$0,10
+     syscall
